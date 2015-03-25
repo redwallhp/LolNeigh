07caee6dfe347c7aa656c466b9d54455407e6a67
redwallhp <mcredwallhp@gmail.com>
2015-03-25 18:40:41 +1000

    Added bed mode, reload command and logging.

--- src/main/java/io/github/mcredwallhp/lolneigh/LolNeigh.java
+++ src/main/java/io/github/mcredwallhp/lolneigh/LolNeigh.java
@@ -3,13 +3,13 @@ package io.github.mcredwallhp.lolneigh;
 import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityPortalEnterEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.Listener;
+
+import java.util.UUID;
 
 public final class LolNeigh extends JavaPlugin implements Listener {
 
@@ -18,16 +18,15 @@ public final class LolNeigh extends JavaPlugin implements Listener {
     Double horseRespawnX;
     Double horseRespawnY;
     Double horseRespawnZ;
+    Boolean bedMode;
+    Boolean logThings;
 
 
     @Override
     public void onEnable() {
         this.saveDefaultConfig();
         this.getServer().getPluginManager().registerEvents(this, this);
-        this.horseRespawnWorld = this.getConfig().getString("horse_respawn.world", "world");
-        this.horseRespawnX = this.getConfig().getDouble("horse_respawn.x", 0);
-        this.horseRespawnY = this.getConfig().getDouble("horse_respawn.y", 70);
-        this.horseRespawnZ = this.getConfig().getDouble("horse_respawn.z", 0);
+        this.loadConfigOptions();
     }
 
 
@@ -36,7 +35,7 @@ public final class LolNeigh extends JavaPlugin implements Listener {
 
         Player player = (Player) sender;
 
-        if (!player.hasPermission("lolneigh.set")) {
+        if (!player.hasPermission("lolneigh.admin")) {
             sender.sendMessage(ChatColor.RED + "You do not have permission to do that.");
             return true;
         }
@@ -47,8 +46,25 @@ public final class LolNeigh extends JavaPlugin implements Listener {
             return true;
         }
 
+        if (cmd.getName().equalsIgnoreCase("lolneigh-reload")) {
+            this.reloadConfig();
+            this.loadConfigOptions();
+            player.sendMessage(ChatColor.GOLD + "LolNeigh config reloaded!");
+            return true;
+        }
+
         return false;
 
+    }
+
+
+    public void loadConfigOptions() {
+        this.horseRespawnWorld = this.getConfig().getString("horse_respawn.world", "world");
+        this.horseRespawnX = this.getConfig().getDouble("horse_respawn.x", 0);
+        this.horseRespawnY = this.getConfig().getDouble("horse_respawn.y", 70);
+        this.horseRespawnZ = this.getConfig().getDouble("horse_respawn.z", 0);
+        this.bedMode = this.getConfig().getBoolean("bed_mode", false);
+        this.logThings = this.getConfig().getBoolean("log_things", true);
     }
 
 
@@ -73,19 +89,87 @@ public final class LolNeigh extends JavaPlugin implements Listener {
 
         // If entity exiting portal is a horse, and the world type is the End, handle the teleport
         if (entity.getType() == EntityType.HORSE && portalLoc.getWorld().getEnvironment() == World.Environment.THE_END) {
-
-            World toWorld = Bukkit.getWorld(this.horseRespawnWorld);
-            Location toLoc = new Location(toWorld, this.horseRespawnX, this.horseRespawnY, this.horseRespawnZ);
-
-            // Ensure the chunk is loaded before teleporting
-            Chunk toChunk = toLoc.getChunk();
-            if (!toChunk.isLoaded()) {
-                toChunk.load();
+            if (this.bedMode) {
+                this.teleportHorseToPlayerBed(entity);
             }
-
-            entity.teleport(toLoc);
-
+             else {
+                this.teleportHorseToCoordinates(entity);
+            }
         }
+
+    }
+
+
+    public void teleportHorseToCoordinates(Entity horse) {
+
+        World toWorld = Bukkit.getWorld(this.horseRespawnWorld);
+        Location toLoc = new Location(toWorld, this.horseRespawnX, this.horseRespawnY, this.horseRespawnZ);
+
+        // Ensure the chunk is loaded before teleporting
+        Chunk toChunk = toLoc.getChunk();
+        if (!toChunk.isLoaded()) {
+            toChunk.load();
+        }
+
+        horse.teleport(toLoc);
+
+        this.logHorseEvent(horse, false);
+
+    }
+
+
+    public void teleportHorseToPlayerBed(Entity horse) {
+
+        Horse h = (Horse) horse;
+        AnimalTamer tamer = h.getOwner();
+
+        // Horse has no owner, teleport to predefined spawn
+        if (tamer == null) {
+            this.teleportHorseToCoordinates(horse);
+            this.logHorseEvent(horse, false);
+            return;
+        }
+
+        // Teleport horse to player bed, falling back to predetermined spawn if not set
+        UUID ownerUUID = tamer.getUniqueId();
+        OfflinePlayer owner = Bukkit.getOfflinePlayer(ownerUUID);
+        Location bedLoc = owner.getBedSpawnLocation();
+        if (bedLoc != null) {
+            horse.teleport(bedLoc);
+            this.logHorseEvent(horse, true);
+        } else {
+            this.teleportHorseToCoordinates(horse);
+            this.logHorseEvent(horse, false);
+        }
+
+    }
+
+
+    public void logHorseEvent(Entity entity, Boolean isGoingToBed) {
+
+        if (!this.logThings) return;
+
+        String msg;
+        String owner;
+        String owner_uuid;
+        Horse h = (Horse) entity;
+        AnimalTamer tamer = h.getOwner();
+
+        if (tamer == null) {
+            owner = "nobody";
+            owner_uuid = "untamed";
+        } else {
+            owner = tamer.getName();
+            owner_uuid = tamer.getUniqueId().toString();
+        }
+
+        if (isGoingToBed) {
+            msg = String.format("[LolNeigh] Teleporting horse owned by %s (%s) to player bed.", owner, owner_uuid);
+        } else {
+            msg = String.format("[LolNeigh] Teleporting horse owned by %s (%s) to respawn point.", owner, owner_uuid);
+        }
+
+        Bukkit.getLogger().info(msg);
 
     }
 
